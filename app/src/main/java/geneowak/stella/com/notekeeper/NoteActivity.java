a544479d94bdf14a2ebf4292f59d7601f4347e1c
Eugene Owak <teugeneowak@gmail.com>
2019-05-06 11:39:44 +0300

    - Simplify activity interaction within the app

--- app/src/main/java/geneowak/stella/com/notekeeper/NoteActivity.java
+++ app/src/main/java/geneowak/stella/com/notekeeper/NoteActivity.java
@@ -13,7 +13,8 @@ import android.widget.Spinner;
 import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
-    public static final String NOTE_INFO = "geneowak.stella.com.notekeeper.NOTE_INFO";
+    public static final String NOTE_POSITION = "geneowak.stella.com.notekeeper.NOTE_POSITION";
+    public static final int POSITION_NOT_SET = -1;
     private NoteInfo mNote;
     private boolean mIsNewNote;
 
@@ -55,8 +56,11 @@ public class NoteActivity extends AppCompatActivity {
 
     private void readDisplayStateValues() {
         Intent intent = getIntent();
-        mNote = intent.getParcelableExtra(NOTE_INFO);
-        mIsNewNote = mNote == null;
+        int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
+        mIsNewNote = position == POSITION_NOT_SET;
+
+        if (!mIsNewNote)
+            mNote = DataManager.getInstance().getNotes().get(position);
     }
 
     @Override
--- app/src/main/java/geneowak/stella/com/notekeeper/NoteListActivity.java
+++ app/src/main/java/geneowak/stella/com/notekeeper/NoteListActivity.java
@@ -45,8 +45,8 @@ public class NoteListActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
 
-                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-                intent.putExtra(NoteActivity.NOTE_INFO, note);
+//                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+                intent.putExtra(NoteActivity.NOTE_POSITION, position);
 
                 startActivity(intent);
             }
