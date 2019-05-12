8375aaa58c31123638955bd6e2461994bcf1a6c4
Eugene Owak <teugeneowak@gmail.com>
2019-05-12 19:03:57 +0300

    - Add Note creation UI tests - Add Logic verification for note creation - Add NoteListActivityTest using android studio test recording

---
+++ app/src/androidTest/java/geneowak/stella/com/notekeeper/NoteCreationTest.java
@@ -0,0 +1,72 @@
+package geneowak.stella.com.notekeeper;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Owak
+ */
+@RunWith(AndroidJUnit4.class)
+public class NoteCreationTest {
+    static DataManager sDataManager;
+    @Rule
+    public ActivityTestRule<NoteListActivity> eNoteListActivityActivityRule =
+            new ActivityTestRule<>(NoteListActivity.class);
+
+    @BeforeClass
+    public static void classSetUp() throws Exception {
+        sDataManager = DataManager.getInstance();
+    }
+
+    @Test
+    public void createNewNote() {
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of our test note.";
+//        ViewInteraction fabNewNote = onView(withId(R.id.fab));
+//        fabNewNote.perform(click());
+        onView(withId(R.id.fab)).perform(click());
+
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(containsString(course.getTitle()))));
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle)).check(matches(withText(containsString(noteTitle))));
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
+                closeSoftKeyboard());
+
+        onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
+
+        pressBack();
+
+        // assert that a new note was created
+        int noteIndex = sDataManager.getNotes().size() - 1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
+    }
+
+}
