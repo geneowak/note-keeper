cd6641e93c031e234d3650539ad3090412fd2f93
geneowak <teugeneowak@gmail.com>
2019-10-12 17:16:24 +0300

    - Create Content Provider. - Add content provider to app. - Implement Content Provider over SQLite. - Request data from the content provider. - Load course spinner from a content provider. - Load course data from content provider. - Verify data interaction from another app.

--- app/src/test/java/geneowak/stella/com/notekeeper/ExampleUnitTest.java
+++ app/src/test/java/geneowak/stella/com/notekeeper/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package geneowak.stella.com.notekeeper;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
